bdef1095af11b28baed3b2da5e04718f7f19651d
MartinOlof <94788540+MartinOlof@users.noreply.github.com>
2024-10-21 07:16:08 +0200

    Update MagnetSensor.c

--- MagnetSensor.c
+++ MagnetSensor.c
@@ -1,3 +1,8 @@
+#include <xc.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define ADC_VALUE 614 //3V
 #define LED_INDICATOR PB4
 #define Q_GATE PB3
 DDRB =  0x18 // PB3 och PB4 som output 0b011000
@@ -16,10 +21,10 @@ int main(void)
       uint16_t adcConvert(void);            
     while(1){
             if(adcConvert(val) >= ADC_VALUE){
-		PORTB |= (1 << PB3) | (1 << PB4); 
+		PORTB |= (1 << Q_GATE) | (1 << LED_INDICATOR); 
 	    }
 	    else{
-		PORTB ~= (1 << PB3) | (1 << PB4);
+		PORTB ~= (1 << Q_GATE) | (1 << LED_INDICATOR);
 	    }
     }
 }
@@ -28,6 +33,7 @@ void setupADC(){
       ADMUX |= (1 << MUX0); //Vcc som ref och ADC1 PB2
       ADCSRA = (1 << ADEN) | (1 << ADIE) | (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2); // ADC Enable, prescaler 128bit
       DIDR0 = (1 << ADC1D); //disable digital input
+      ADIE = (1 << ADIE); // Enable ADC Interrupt
       
 
 }
@@ -41,3 +47,4 @@ uint16_t adcConvert(void){
 	uint16_t val = ((adch << 8) | adcl) & 0x3FF;
 	return val;
 }
+
