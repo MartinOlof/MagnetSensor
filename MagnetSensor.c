d42d1c7ba59044dec2984b847ef80627388ee9bf
MartinOlof <94788540+MartinOlof@users.noreply.github.com>
2024-10-23 18:21:54 +0200

    Update MagnetSensor.c

--- MagnetSensor.c
+++ MagnetSensor.c
@@ -1,73 +1,67 @@
+#include <xc.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#define F_CPU 16000000UL
+#include <stdint.h>
 
 #define ADC_VALUE 614 //3V
-#define LED_INDICATOR_ON PORTB |= (1 << PB4)
-#define LED_INDICATOR_OFF PORTB &= ~(1 << PB4)
+#define LED_IND_ON PORTB |= (1 << PB4)
+#define LED_IND_OFF PORTB &= ~(1 << PB4)
 #define Q_GATE_ON PORTB |= (1 << PB3)
 #define Q_GATE_OFF PORTB &= ~(1 << PB3)
-//DDRB =  0x018 // PB3 och PB4 som output 0b011000
+uint16_t adcConvert();
 void setupADC();
-uint16_t adcConvert(void);
-sei(); // enable global interrupt
-void WDT_OFF(void); // Turn off WDT
-void idleSleep();
 
 
 int main(void)
 {
-      setupADC();
-      uint16_t adcConvert(void);            
-    while(1){
-            if(adcConvert(val) >= ADC_VALUE){
-		EMPTY_INTERRUPT(ADC_vect);
-		LED_INDICATOR_ON;
-		Q_GATE_ON;
-	    }
-	    else{
-		LED_INDICATOR_OFF; 
-		Q_GATE_OFF;
-		void idleSleep();
-	    }
+	setupADC();
+	DDRB |= (1 << PB3) | (1 << PB4);
+	//PORTB &= ~(1 << PB5);
+	
+    while(1)
+    {
+		
+		uint16_t val = adcConvert();
+		if(val >= ADC_VALUE){
+			Q_GATE_ON;
+			LED_IND_ON;
+			Q_GATE_OFF;
+			LED_IND_OFF;
+		}
+		else{
+			Q_GATE_OFF;
+			LED_IND_OFF;
+		}
     }
 }
 
 void setupADC(){
-      ADMUX |= (1 << MUX0); //Vcc som ref och ADC1 PB2
-      ADCSRA = (1 << ADEN) | (1 << ADIE) | (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2); // ADC Enable, prescaler 128bit
-      DIDR0 = (1 << ADC1D); //disable digital input, behövs den ens?
-      ADCSRA = (1 << ADIE); // Enable adc interrupt
-
+	ADMUX |= (0 << MUX0) | (0 << MUX1) | (0 << MUX2) | (0 << MUX3);
+	ADCSRA = (1 << ADEN) | (1 << ADIE) | (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2);
+	//DIDR0 = (1 << ADC1D);
+	ADMUX |= (0 << REFS0);
+	
+	
 }
 
-uint16_t adcConvert(void){
+uint16_t adcConvert(){
+	/*ADCSRA |= (1 << ADSC);
+	while (ADCSRA & (1 << ADSC));
+	uint16_t adc_val = (ADCH << 8) | ADCL;
+	
+	
+	return adc_val;*/
+	
+	int Ain, AinLow;
 	ADCSRA |= (1 << ADSC);
-
-	while ((ADCSRA & (1 << ADSC)) == 1){}
-	uint8_t adcl = ADCL;
-	uint8_t adch = ADCH;
-	uint16_t val = ((adch << 8) | adcl) & 0x3FF;
-	return val;
-}
-
-/*
-ISR(ADC_vect){
-	adc_flag = 1;
-}
-*/
-
-
-
-void WDT_OFF(void){
-	_WDR();
-
-	MCUSR = 0x00;
-	WDTCR |= (1 << WDCE) | (1 << WDE);
-	WDTCR = 0x00;
-}
-
-void idleSleep(){
-
-	MCUCR |= (1 << SE) | (0 << SM1) | (0 << SM0); //Sleep mode EN, IDLE mode set
+	
+	while(ADCSRA &(ADIF) == 0);
+	
+	AinLow = (int)ADCL;
+	Ain = (int)ADCH*256;
+	
+	Ain += AinLow;
+	
+	return Ain;
+	
 }
